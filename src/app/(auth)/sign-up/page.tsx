605b6d03b0f4ab01889ddd0f04f84bfe43bf1fb5
balbader <basilalbapro@gmail.com>
2024-11-11 11:12:58 +0100

    linting

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -6,12 +6,12 @@ import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
 } from "@/components/ui/form";
 import { pageTitleStyles } from "@/styles/common";
 import { cn } from "@/lib/utils";
@@ -23,118 +23,118 @@ import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Terminal } from "lucide-react";
 
 const registrationSchema = z
-  .object({
-    email: z.string().email(),
-    password: z.string().min(8),
-    passwordConfirmation: z.string().min(8),
-  })
-  .refine((data) => data.password === data.passwordConfirmation, {
-    message: "Passwords don't match",
-    path: ["passwordConfirmation"],
-  });
+    .object({
+        email: z.string().email(),
+        password: z.string().min(8),
+        passwordConfirmation: z.string().min(8),
+    })
+    .refine((data) => data.password === data.passwordConfirmation, {
+        message: "Passwords don't match",
+        path: ["passwordConfirmation"],
+    });
 
 export default function RegisterPage() {
-  const { toast } = useToast();
+    const { toast } = useToast();
 
-  const { execute, isPending, error } = useServerAction(signUpAction, {
-    onError({ err }) {
-      toast({
-        title: "Something went wrong",
-        description: err.message,
-        variant: "destructive",
-      });
-    },
-  });
+    const { execute, isPending, error } = useServerAction(signUpAction, {
+        onError({ err }) {
+            toast({
+                title: "Something went wrong",
+                description: err.message,
+                variant: "destructive",
+            });
+        },
+    });
 
-  const form = useForm<z.infer<typeof registrationSchema>>({
-    resolver: zodResolver(registrationSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-      passwordConfirmation: "",
-    },
-  });
+    const form = useForm<z.infer<typeof registrationSchema>>({
+        resolver: zodResolver(registrationSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+            passwordConfirmation: "",
+        },
+    });
 
-  function onSubmit(values: z.infer<typeof registrationSchema>) {
-    execute(values);
-  }
+    function onSubmit(values: z.infer<typeof registrationSchema>) {
+        execute(values);
+    }
 
-  return (
-    <div className="py-24 mx-auto max-w-[400px] space-y-6">
-      <h1 className={cn(pageTitleStyles, "text-center")}>Sign Up</h1>
+    return (
+        <div className="py-24 mx-auto max-w-[400px] space-y-6">
+            <h1 className={cn(pageTitleStyles, "text-center")}>Sign Up</h1>
 
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    className="w-full"
-                    placeholder="Enter your email"
-                    type="email"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        {...field}
+                                        className="w-full"
+                                        placeholder="Enter your email"
+                                        type="email"
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
 
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    className="w-full"
-                    placeholder="Enter your password"
-                    type="password"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        {...field}
+                                        className="w-full"
+                                        placeholder="Enter your password"
+                                        type="password"
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
 
-          <FormField
-            control={form.control}
-            name="passwordConfirmation"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Confirm Password</FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    className="w-full"
-                    placeholder="Enter Confirm your Password"
-                    type="password"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+                    <FormField
+                        control={form.control}
+                        name="passwordConfirmation"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Confirm Password</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        {...field}
+                                        className="w-full"
+                                        placeholder="Enter Confirm your Password"
+                                        type="password"
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
 
-          {error && (
-            <Alert variant="destructive">
-              <Terminal className="h-4 w-4" />
-              <AlertTitle>Uh-oh, we couldn&apos;t log you in</AlertTitle>
-              <AlertDescription>{error.message}</AlertDescription>
-            </Alert>
-          )}
+                    {error && (
+                        <Alert variant="destructive">
+                            <Terminal className="h-4 w-4" />
+                            <AlertTitle>Uh-oh, we couldn&apos;t log you in</AlertTitle>
+                            <AlertDescription>{error.message}</AlertDescription>
+                        </Alert>
+                    )}
 
-          <LoaderButton isLoading={isPending} className="w-full" type="submit">
-            Register
-          </LoaderButton>
-        </form>
-      </Form>
-    </div>
-  );
+                    <LoaderButton isLoading={isPending} className="w-full" type="submit">
+                        Register
+                    </LoaderButton>
+                </form>
+            </Form>
+        </div>
+    );
 }
